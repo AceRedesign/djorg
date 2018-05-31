b404a0b0a9edbfbf9c94b41e7aa8704580b116d7
aceredesign <steven.magadan@hotmail.com>
2018-05-31 18:52:17 -0400

    added notes

--- notes/api.py
+++ notes/api.py
@@ -11,3 +11,15 @@ class NoteSerializer(serializers.HyperlinkedModelSerializer):
         user = self._context['request'].user
         note = Note.objects.create(user=user, **validated_data)
         return note
+
+
+class NoteViewSet(viewsets.ModelViewSet):
+    serializer_class = NoteSerializer
+    queryset = Note.objects.none()
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_anonymous:
+            return Note.objects.none()
+        else:
+            return Note.objects.filter(user=user)
